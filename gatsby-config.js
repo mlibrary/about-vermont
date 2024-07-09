4cf2d1d33a33bbd55d8c77ae1391689ae7f90670
jmcglone <jmcglone@umich.edu>
2024-07-09 16:26:27 -0400

    case sensitive files

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/VermontLogo-Badge.png`, // This path is relative to the root of the site.
+        icon: `src/images/VermontLogo-badge.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
